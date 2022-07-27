86208b4b3d0aa436a7ffca07e1aa96430aaa5a74
B0923016 <pitt20020608@gmail.com>
2022-07-27 19:26:14 +0800

    22

---
+++ name.c.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    int arr_write[5] = {1, 3, 5, 7, 9};
+
+
+}   
---
+++ okkk.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    int abc[5] = {1, 3, 5, 7, 9};
+    int a;
+    scanf("%d", a);
+
+}   
